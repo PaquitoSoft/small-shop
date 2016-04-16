dee653eeeb5d70c18721118107914db1edb64611
PaquitoSoft <paquitosoftware@gmail.com>
2016-04-16 12:26:19 +0200

    Tests barebones

---
+++ spec/src/routes/catalog.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Catalog routes', function() {
+
+	it('Should return all categories');
+
+	it('Should return a category details');
+
+	it('Should return featured product list');
+
+	it('Should return a category products');
+
+	it('Should return a product details');
+
+});
